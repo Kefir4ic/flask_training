8847368a034cdccd42149b073ec36b8125ef06d3
Maxim_Tikhomirov <tvn1212yandex2@gmail.com>
2022-06-24 14:43:23 +0300

    Окончание 3 занятия

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
-@app.route('/new')
 def index():
-    user = {'username': 'Эльдар Рязанов'}
+    user = {'username': 'Максим Тихомиров'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -23,5 +24,20 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
-    # return "Hello, world!"
-    # return "Привет, мир!"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/say_hello')
+def say_hello():
+    english_hello = "Hello, world!"
+    russian_hello = "Привет, мир!"
+    return render_template('say_hello.html', title='Say Hello', english_hello=english_hello, russian_hello=russian_hello)

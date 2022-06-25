294798717f3027413178828f5f6f3e215a150c52
Maxim_Tikhomirov <tvn1212yandex2@gmail.com>
2022-06-25 12:57:34 +0300

    добавление комментариев для config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+# файл конфигурации приложения
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # настройка ссылки на базу данных, в которой хранятся данные пользователей и посты, написанные на сайте
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
